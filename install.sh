5a88e219b608b53667b99adb7c8330ab3fa8a11f
raviqqe <raviqqe@gmail.com>
2015-04-02 01:00:51 +0000

    packages.list format changed

--- install.sh
+++ install.sh
@@ -2,18 +2,25 @@
 
 echo "install.sh - initial package installation script"
 
+# install pkg command when it's not done yet
+yes | pkg
+
 cat packages.list |
 sed 's/#.*//g' |
 grep -v '^[[:blank:]]*$' |
 while read word1 word2
 do
-	if [ -z $word2 ]
-	then
-		echo "installing $word1"
-		pkg install -y $word1
-	elif [ $word1 == 'r' ]
-	then
+  case word1 in
+  i)
+		echo "installing $word2"
+		pkg install -y $word2
+    ;;
+  r)
 		echo "removing $word2"
 		pkg remove -y $word2
-	fi
+    ;;
+  *)
+    echo invalid letter at the first field
+    exit 1
+  esac
 done
