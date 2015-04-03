4e6a0514b46f85009f5ebd1caadae82a4f1877a7
raviqqe <raviqqe@gmail.com>
2015-04-03 09:40:09 +0000

    install_conf.sh separated from install.sh

---
+++ install_conf.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+error() {
+  echo "$*" >&2
+}
+
+fail() {
+  error "$*"
+  exit 1
+}
+
+echo "install_conf.sh - initial package installation script"
+
+if [ $# -ne 1 ]
+then
+  fail "usage: $0 <home_directory>"
+fi
+
+USER=$(stat -f %Su "$1")
+GROUP=$(stat -f %Sg "$1")
+readonly USER GROUP
+
+# copy config files
+cd files
+mkdir tmp
+for file in $(ls | sed 's/tmp//g')
+do
+  cp -R $file tmp/$(echo "$file" | sed 's/DUMMY//g')
+done
+(
+  cd tmp
+  install -o "$USER" -g "$GROUP" -m 600 $(ls -A) /home/"$USER"
+)
+rm -rf tmp
