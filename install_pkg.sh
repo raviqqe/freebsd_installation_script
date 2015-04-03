32c71e22e460c8527fa848997f5e9ce62b05970c
raviqqe <raviqqe@gmail.com>
2015-04-03 09:44:29 +0000

    script debugged

--- install_pkg.sh
+++ install_pkg.sh
@@ -9,11 +9,7 @@ fail() {
   exit 1
 }
 
-USER=$(stat -f %Su "$1")
-GROUP=$(stat -f %Sg "$1")
-readonly USER GROUP
-
-echo "install.sh - initial package installation script"
+echo "install_pkg.sh - initial package installation script"
 
 # install pkg command when it's not done yet
 if [ -z "$(which pkg)" ]
@@ -24,38 +20,31 @@ fi
 # install packages
 cat packages.list |
 sed 's/#.*//g' |
-grep -v '^[[:blank:]]*$' |
-while read word1 word2 others
-do
-  if [ -n "$others" ]
-  then
-    fail "too many fields in packages.list: $word1 $word2 $others"
-  fi
-
-  case $word1 in
-  i)
-    pkg_installed="$pkg_installed $word1"
-    ;;
-  r)
-    pkg_removed="$pkg_removed $word2"
-    ;;
-  *)
-    fail "invalid letter at the first field: $word1"
-  esac
-done
-
-pkg install -y $pkg_installed
-pkg remove -y $pkg_removed
-
-# copy config files
-cd files
-mkdir tmp
-for file in $(ls)
-do
-  mv $file tmp/$(echo "$file" | sed 's/DUMMY//g')
-done
-(
-  cd tmp
-  install -o "$USER" -g "$GROUP" -m 600 $(ls -A) /home/"$USER"
+grep -v '^[[:blank:]]*$' | (
+  while read word1 word2 others
+  do
+    if [ -n "$others" ]
+    then
+      fail "too many fields in packages.list: $word1 $word2 $others"
+    fi
+  
+    case $word1 in
+    i)
+      pkg_installed="$pkg_installed $word2"
+      ;;
+    r)
+      pkg_removed="$pkg_removed $word2"
+      ;;
+    *)
+      fail "invalid letter at the first field: $word1"
+    esac
+  done
+  echo $pkg_installed > pkg_installed
+  echo $pkg_removed > pkg_removed
 )
-rm -rf tmp
+
+test -s pkg_installed && pkg install -y $(cat pkg_installed)
+test -s pkg_removed && pkg delete -y $(cat pkg_removed)
+
+test -f pkg_installed && rm pkg_installed
+test -f pkg_removed && rm pkg_removed
